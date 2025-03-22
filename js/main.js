66af34afd74eb0b6aa16646d1ab3643f7e6cbd01
ryzhkoandrey <ryzhkoandrey@gmail.com>
2025-03-22 22:23:56 +0200

    +

--- js/main.js
+++ js/main.js
@@ -1,3 +1,15 @@
 'use strict';
 
-// ----------  ----------
+// ---------- NEWS ----------
+
+// Swiper
+
+const swiper = new Swiper('#news-swiper', {
+   slidesPerView: 3,
+   spaceBetween: 30,
+
+   navigation: {
+      prevEl: ".swiper-btn-prev",
+      nextEl: ".swiper-btn-next",
+   },
+});
